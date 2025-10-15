7bd1373d64d33b9dfedd6df0d76ec62d746e6ea9
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-10-14 19:13:04 -0500

    Update info-grupos-lista.js

--- plugins/Propietario/info-grupos-lista.js
+++ plugins/Propietario/info-grupos-lista.js
@@ -1,26 +1,40 @@
 import PhoneNumber from 'awesome-phonenumber'
+
 let handler = async (m, { conn, __dirname, isRowner, isOwner, isBotAdmin, usedPrefix, groupMetadata, groups, _package, participants }) => { 
-const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
+  const fkontak = { 
+    "key": { 
+      "participants":"0@s.whatsapp.net", 
+      "remoteJid": "status@broadcast", 
+      "fromMe": false, 
+      "id": "Halo" 
+    }, 
+    "message": { 
+      "contactMessage": { 
+        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+      }
+    }, 
+    "participant": "0@s.whatsapp.net"
+  }
+
+  const chats = Object.entries(conn.chats).filter(([jid, data]) => jid && data.isChats)
+  groups = Object.values(await conn.groupFetchAllParticipating())
+
+  let txt = `📋 *LISTA DE GRUPOS DEL BOT*\n\n🧩 Total de grupos: ${groups.length}\n`
+
+  for (let i = 0; i < groups.length; i++) {
+    txt += `
+🏷️ *Nombre:* ${groups[i].subject}
+🆔 *ID:* ${groups[i].id}
+👥 *Miembros:* ${groups[i].participants.length}\n`
+  }
+
+  m.reply(txt.trim())
 }
 
-let txt
-const chats = Object.entries(conn.chats).filter(([jid, data]) => jid && data.isChats)
-groups = Object.values(await conn.groupFetchAllParticipating())
-txt = `${packname} ${lenguajeGB.smsLisA()}
-${lenguajeGB.smsLisB()} ${groups.length}\n`
-
-for (let i = 0; i < groups.length; i++) {
-txt += ` 
-${lenguajeGB.smsLisC()} ${groups[i].subject}
-${lenguajeGB.smsLisD()} ${groups[i].id}
-${isOwner ? `${lenguajeGB.smsLisE()} ${groups[i].participants.length}` : ''}\n\n`
-}
-m.reply(txt.trim())
-}
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
 handler.command = /^(groups|grouplist|listadegrupo|gruposlista|listagrupos|listadegrupos|grupolista|listagrupo)$/i
-handler.rowner = true;
+handler.rowner = true
 handler.exp = 30
-export default handler
 
+export default handler
