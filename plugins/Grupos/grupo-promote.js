07cdcf9ac46b7e0d989fb9de67f69a98bbfb6bbf
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-02-21 08:55:17 -0500

    Refactor promote handler for better clarity

--- plugins/Grupos/grupo-promote.js
+++ plugins/Grupos/grupo-promote.js
@@ -1,33 +1,31 @@
-let handler = async (m, {conn, usedPrefix, command, text}) => {
+const handler = async (m, {conn, usedPrefix, text}) => {
 if (isNaN(text) && !text.match(/@/g)) {
 } else if (isNaN(text)) {
-var number = text.split`@`[1]
+var number = text.split`@`[1];
 } else if (!isNaN(text)) {
-var number = text
+var number = text;
 }
 
-if (!text && !m.quoted) return conn.reply(m.chat, lenguajeGB.smsMalused3(), +`*${usedPrefix + command} @${global.owner[0][0]}*`, fkontak, m)
-//conn.sendButton(m.chat, wm, lenguajeGB['smsMalused3']() + `*${usedPrefix + command} @${global.owner[0][0]}*`, null, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], fkontak, m)
-if (number.length > 13 || (number.length < 11 && number.length > 0))
-return conn.reply(m.chat, lenguajeGB.smsDemott(), `*${usedPrefix + command} @${global.owner[0][0]}*`, fkontak, m)
-//conn.sendButton(m.chat, wm, lenguajeGB['smsDemott']() + `*${usedPrefix + command} @${global.owner[0][0]}*`, null, [[lenguajeGB.smsConMenu(), `${usedPrefix}menu`]], fkontak, m)
-
+if (!text && !m.quoted) return conn.reply(m.chat, `*⚠️ ¿A quien le doy admins?* Etiqueta a una persona no soy adivino :)`, m);
+if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*⚠️ Estas drogado ese número ingresado es incorrecto 🤓*, ingresar un número correcto o mejor etiquetas al usuario @tag`, m);
 try {
 if (text) {
-var user = number + '@s.whatsapp.net'
+var user = number + '@s.whatsapp.net';
 } else if (m.quoted.sender) {
-var user = m.quoted.sender
+var user = m.quoted.sender;
 } else if (m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-}
-} catch (e) {
+var user = number + '@s.whatsapp.net';
+}} catch (e) {
 } finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-conn.reply(m.chat, lenguajeGB['smsAvisoEG']() + lenguajeGB['smsDemott2'](), fkontak, m)
-}
-}
-handler.command = /^(promote|daradmin|darpoder)$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-export default handler
+conn.groupParticipantsUpdate(m.chat, [user], 'promote');
+conn.reply(m.chat, `*[ ✅ ] ÓRDENES RECIBIDAS*`, m);
+}};
+handler.help = ['*593xxx*', '*@usuario*', '*responder chat*'].map((v) => 'promote ' + v);
+handler.tags = ['group'];
+handler.command = /^(promote|daradmin|darpoder)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+handler.fail = null;
+handler.register = true 
+export default handler;
