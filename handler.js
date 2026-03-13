7e8ba3e67eaf81c18961d0b0c9a5e3c67e999839
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-03-13 14:41:16 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1765,7 +1765,7 @@ export async function participantsUpdate({ id, participants, action }) {
 case 'remove':
 
 		        
-/*if (chat.welcome) {
+if (chat.welcome) {
 let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata
 for (let user of participants) {
 let pp = './src/sinfoto.jpg'
@@ -1778,7 +1778,7 @@ const botTt2 = groupMetadata.participants.find(u => this.decodeJid(u.id) == this
 const isBotAdminNn = botTt2?.admin === "admin" || false
 text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc?.toString() || '𝑆𝐼𝑁 𝐷𝐸𝑆𝐶𝑅𝐼𝑃𝐶𝐼𝑂́𝑁 ') :
 (chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', '@' + user.split('@')[0])
-*/
+
 
 
 
