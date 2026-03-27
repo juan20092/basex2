53ba7fab3999e6a28ae8c816d4cc16b3d5d3c82e
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-03-27 11:17:53 -0500

    Update antipriv.js

--- plugins/Grupos/antipriv.js
+++ plugins/Grupos/antipriv.js
@@ -1,34 +1,20 @@
-import fs from 'fs'
+export async function before(m, {isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes("PIEDRA") || m.text.includes("PAPEL") || m.text.includes("TIJERA")) return !0;
+  let bot = global.db.data.settings[this.user.jid] || {};
 
-export async function before(m, { isOwner, isROwner }) {
-  if (m.isBaileys && m.fromMe) return true
-  if (!m.message) return true
-  if (m.isGroup) return false
-
-  let user = m.sender.split(':')[0]
-
-  let bot = global.db.data.settings[this.user.jid] || {}
-  if (!bot.antiPrivate) return false
-  if (isOwner || isROwner) return false
-
-  // 🔥 SI YA ESTÁ BLOQUEADO (IGNORAR)
-  if (global.db.data.users[user]?.banned) return true
-
-  let numero = user.split('@')[0]
-
-  await this.sendMessage(m.chat, {
-    text: `🚫 No puedes hablar al privado del bot.\n\nSerás ignorado automáticamente.`,
-    mentions: [user]
-  }, { quoted: m })
-
-  // 🔥 "BLOQUEO REAL" (IGNORAR)
-  if (!global.db.data.users[user]) {
-    global.db.data.users[user] = {}
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    await m.reply(
+      `» 𝗘𝗦𝗧𝗔 𝗣𝗥𝗢𝗛𝗜𝗕𝗜𝗗𝗢 𝗘𝗦𝗖𝗥𝗜𝗕𝗜𝗥𝗟𝗘 𝗔𝗟 𝗕𝗢𝗧 𝗔𝗟 𝗣𝗥𝗜𝗩𝗔𝗗𝗢, 𝗦𝗘𝗥𝗔́𝗦 𝗕𝗟𝗢𝗤𝗨𝗘𝗔𝗗𝗢/𝗔 .
+﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
+» 𝗦𝗜 𝗡𝗘𝗖𝗘𝗦𝗜𝗧𝗔𝗦 𝗨𝗡 𝗕𝗢𝗧 𝗢 𝗧𝗜𝗘𝗡𝗘𝗦 𝗔𝗟𝗚𝗨́𝗡 𝗜𝗡𝗖𝗢𝗡𝗩𝗘𝗡𝗜𝗘𝗡𝗧𝗘 𝗖𝗢𝗡𝗧𝗔́𝗖𝗧𝗔𝗧𝗘 𝗖𝗢𝗡 𝗠𝗜 𝗖𝗥𝗘𝗔𝗗𝗢𝗥.
+» Wa.me/593993370003 ${md}`,
+      false,
+      {mentions: [m.sender]}
+    );
+    await this.updateBlockStatus(m.chat, "block");
   }
-
-  global.db.data.users[user].banned = true
-
-  console.log("🚫 Usuario ignorado:", user)
-
-  return true
+  return !1;
 }
